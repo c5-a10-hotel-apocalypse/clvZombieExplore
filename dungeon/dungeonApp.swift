e5d3efab4096ea2db6f573c8d4360235f3430a9a
Calvin Christian Tjong <calvinchristiant3@gmail.com>
2025-07-10 16:52:16 +0700

    Initial Commit

---
+++ dungeon/dungeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  dungeonApp.swift
+//  dungeon
+//
+//  Created by Calvin Christian Tjong on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct dungeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
